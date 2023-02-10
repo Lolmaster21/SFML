82ab783bfc7993b60ee6e9eba2d279c91eba7377
dabruhv <78553141+dabruhv@users.noreply.github.com>
2023-02-10 10:00:48 -0700

    Update Breakout.cpp

--- Breakout.cpp
+++ Breakout.cpp
@@ -12,19 +12,47 @@ private:
 public:
 	void init(int x, int y);
 	void DrawB();
+	
 
 
 };
+sf::RenderWindow window(sf::VideoMode(800, 800), "Breakout");
 
-int main(){
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Breakout");
+int main() {
+	
+	Brick block;
+	sf::Event event;
+	float py = 700;
+	float px = 400;
+	block.init(200, 200);
 
-	while (true) {
+	sf::RectangleShape paddle(sf::Vector2f(100.0f, 20.0f));
+	paddle.setFillColor(sf::Color::Blue); 
+	paddle.setPosition(px, py);
 
-		window.display();
+	
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+
+
+
+
+
+
+
+
+
+			//render section------------------------------------------------------------
+
+			window.clear();
+			window.draw(paddle);
+			block.DrawB();
+			window.display();
+		}
 	}
 }
-void Brick:: init(int x, int y) {
+void Brick::init(int x, int y) {
 	xpos = x;
 	ypos = y;
 	width = 100;
@@ -33,7 +61,10 @@ void Brick:: init(int x, int y) {
 }
 
 void Brick::DrawB() {
-
+	sf::RectangleShape rectangle(sf::Vector2f(width, height));
+	rectangle.setFillColor(sf::Color::Red);
+	rectangle.setPosition(xpos, ypos);
+	window.draw(rectangle);
 
 }
 
